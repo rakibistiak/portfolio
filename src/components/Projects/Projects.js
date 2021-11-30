9f6cb43a09851c40207758c1cd4a4e5ea02aa777
rakibemon <rakibemon03@gmail.com>
2021-11-30 15:22:40 +0600

    project section added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,40 +1,41 @@
-import React from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-
-// import Swiper core and required modules
-import SwiperCore, {
-    EffectCoverflow, Pagination
-} from 'swiper';
-
-// install Swiper modules
-SwiperCore.use([EffectCoverflow, Pagination]);
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import './Projects.css'
 const Projects = () => {
+    const navigate = useNavigate()
+    const [projects, setProjects] = useState([]);
+    useEffect(()=>{
+        fetch('/projects.json')
+        .then(res=> res.json())
+        .then(data=> setProjects(data))
+    },[]);
+    const handleDetails = (id) =>{
+        navigate(`project/${id}`)
+    }
     return (
-        <div>
-            <Swiper effect={'coverflow'} grabCursor={true} centeredSlides={true} slidesPerView={'auto'} coverflowEffect={{
-                "rotate": 50,
-                "stretch": 0,
-                "depth": 100,
-                "modifier": 1,
-                "slideShadows": true
-            }} pagination={true} className="mySwiper">
-
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="project" /></SwiperSlide>
-                <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="project" /></SwiperSlide>
-            </Swiper>
-        </div>
+        <Container className='my-5 projet-container'>
+            <Row className='g-4 p-4'>
+                <h2 className='text-center py-5'>My Recent Projects</h2>
+                {
+                    projects.map(project=>{
+                        return(
+                            <Col xs={12}md={6} lg={6} className='single-column'>
+                                <div>
+                                    <figure>
+                                        <img className='project-img p-3' src={project?.img} alt="" />
+                                        <h4 className='text-center'>{project?.title}</h4>
+                                        <div className="text-center">
+                                        <button onClick={()=>handleDetails(project?.id)} className='regular-btn details-btn'>Details</button>
+                                        </div>
+                                    </figure>
+                                </div>
+                            </Col>
+                        );
+                    })
+                }
+            </Row>
+        </Container>
     );
 };
 
